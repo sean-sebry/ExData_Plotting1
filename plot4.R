0298529914dd1f3f78781c2bbaae4ce7207b0075
sean-sebry <sean.sebry@gmail.com>
2019-11-28 20:38:09 +0000

    finalize

--- plot4.R
+++ plot4.R
@@ -9,6 +9,15 @@ glimpse(energy_sub_metering_data)
 
 png("plot4.png")
 
+par(mfrow = c(2, 2))
+
+#plot 1
+with(global_active_power_data, plot(x = date_time, y = global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)"))
+
+#plot 2
+with(global_active_power_data, plot(x = date_time, y = voltage, type = "l", xlab = "datetime", ylab = "Voltage"))
+
+#plot 3
 with(energy_sub_metering_data, plot(x = date_time,
                                     y = sub_metering_1,
                                     type = "l",
@@ -17,5 +26,8 @@ with(energy_sub_metering_data, plot(x = date_time,
 with(energy_sub_metering_data, lines(x = date_time,y = sub_metering_2,type = "l",col = "red"))
 with(energy_sub_metering_data, lines(x = date_time,y = sub_metering_3,type = "l",col = "blue"))
 legend("topright", c("sub_metering_1", "sub_metering_2", "sub_metering_3"), lty = c(1,1,1),col = c("black", "red", "blue"))
+
+#plot 4
+with(global_active_power_data, plot(x = date_time, y = global_reactive_power, type = "l", xlab = "datetime", ylab = "global_reactive_power"))
 
 dev.off()

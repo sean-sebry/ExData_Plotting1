042ba353b65f58be1a85325a5902475fcd8c557a
sean-sebry <sean.sebry@gmail.com>
2019-11-28 20:10:10 +0000

    progress

---
+++ plot4.R
@@ -0,0 +1,21 @@
+energy_sub_metering_data <- data_clean %>%
+  filter(date > ("2007-01-31")) %>%
+  filter(date < ("2007-02-03")) %>% 
+  mutate(date_time = paste(date, time)) %>% 
+  mutate(date_time = ymd_hms(date_time))
+
+glimpse(energy_sub_metering_data)
+
+
+png("plot4.png")
+
+with(energy_sub_metering_data, plot(x = date_time,
+                                    y = sub_metering_1,
+                                    type = "l",
+                                    ylab = "Energy sub metering",
+                                    xlab = ""))
+with(energy_sub_metering_data, lines(x = date_time,y = sub_metering_2,type = "l",col = "red"))
+with(energy_sub_metering_data, lines(x = date_time,y = sub_metering_3,type = "l",col = "blue"))
+legend("topright", c("sub_metering_1", "sub_metering_2", "sub_metering_3"), lty = c(1,1,1),col = c("black", "red", "blue"))
+
+dev.off()
